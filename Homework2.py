ab701d47cac4b5199d6cc6011a3a3b9a8e27c847
Nattaphon <66070280@kmitl.ac.th>
2026-07-03 11:59:45 +0700

    Create Homework2.py

---
+++ Homework2.py
@@ -0,0 +1,10 @@
+color1 = input()
+color2 = input()
+if color1 == "Red" and color2 == "Yellow":
+    print("Orange")
+elif color1 == "Red" and color2 == "Blue":
+    print("Violet")
+elif color1 == "Yellow" and color2 == "Blue":
+    print("Green")
+else:
+    print("Error")
